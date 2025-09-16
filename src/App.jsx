b8b5c0ec8ae79cc9ba7e4431a0bdd0e6c9807805
creavill <reavill.conner@gmail.com>
2025-09-16 15:55:27 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-blue-500 text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-600">Tailwind CSS Test</h1>
+    </div>
+  );
+}
